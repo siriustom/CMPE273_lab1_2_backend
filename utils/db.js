b7614de886252446c9ca34d1ff5aeaf38c3e450d
siriustom <linxiaoran0210@gmail.com>
2018-03-18 10:26:47 -0700

    perform connection pooling for mysql access

--- utils/db.js
+++ utils/db.js
@@ -1,36 +1,42 @@
 var mysql = require('mysql');
 
-getConnection = function() {
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '111111',
-        database: 'user',
-        port: 3306,
-    });
-    return connection;
-}
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'user'
+});
 
 module.exports.fetchData = function(sql, values, callback) {
-    var connection = getConnection();
-    connection.query(sql, [values], function(err, rows, fields) {
-        if(err) {
+    // var connection = getConnection();
+    pool.getConnection(function (err, connection) {
+        if (err) {
             console.log("ERROR: " + err.message);
         } else {
-            callback(err, rows[0]);
+            connection.query(sql, [values], function(err, rows, fields) {
+                if(err) {
+                    console.log("ERROR: " + err.message);
+                } else {
+                    callback(err, rows[0]);
+                }
+            });
         }
+
     });
-    connection.end();
 }
 
 module.exports.updateDate = function (sql, values, callback) {
-    var connection = getConnection();
-    connection.query(sql, values, function(err, rows, fields) {
-        if(err) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
             console.log("ERROR: " + err.message);
         } else {
-            callback(err, rows[0]);
+            connection.query(sql, values, function(err, rows, fields) {
+                if(err) {
+                    console.log("ERROR: " + err.message);
+                } else {
+                    callback(err, rows[0]);
+                }
+            });
         }
-    });
-    connection.end();
+    })
 }
