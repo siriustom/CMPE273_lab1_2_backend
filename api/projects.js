2b7f690e91964829fd2a6123d2e67a07fdde6159
Ubuntu <ubuntu@ip-172-31-22-246.us-west-2.compute.internal>
2018-04-15 06:41:14 +0000

    renaming in post project handler

--- api/projects.js
+++ api/projects.js
@@ -5,40 +5,42 @@ module.exports.postproject = function(req, res) {
     console.log('user has post project');
     var post = req.body;
     console.log(post);
+    var fileName = post.filename;
     var content = {
         userId: post.userId,
         name: post.name,
         title: post.title,
-        des: post.des,
-        skillReq: post.skillReq,
-        budget: post.budget,
-        period: post.period
+        fileName: fileName,
+        description: post.description,
+        skillsRequired: post.skillsRequired,
+        budgetRange: post.budgetRange,
+        period: post.period,
+        averageBid: "",
+        bidNumber: "",
+        bidNow: ""
     }
-
-    var fileName = post.filename;
-
+        var fileName = post.filename;
     var file = req.files.file;
     if(file.mimetype === "application/pdf" ) {
         file.mv('assets/projects/' + fileName, function(err) {
             if (err) return res.status(500).send(err);
+            kafka.makeRequest('postproject', content, function (error, results) {
+                if (error) {
+                    console.log('err: ', error);
+                    return res.send("error happened when post project");
+                }
+                if (!results) {
+                    console.log('no result after post project');
+                    return res.send('db query return no result');
+                }
+                console.log('kafka received normal with project ', results);
+                return res.send('project has been posted');
+            })
         });
     } else {
         var message = "This format is not allowed , please upload file with '.pdf' ";
         return res.send(message);
     }
-
-    kafka.makeRequest('postproject', content, function (err, results) {
-        if (err) {
-            console.log('err: ', err);
-            return res.send("error happened when post project");
-        }
-        if (!results) {
-            console.log('no result after post project');
-            return res.send('db query return no result');
-        }
-        console.log('kafka received normal with project ', results);
-        return res.send('project has been posted');
-    })
 }
 
 module.exports.getAllProjects = function(req, res) {
