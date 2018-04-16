2108ffc31ffe4921c54327103a84befe11e4c4bc
siriustom <linxiaoran0210@gmail.com>
2018-04-15 20:13:37 -0700

    randomly test 5 API

--- test/test.js
+++ test/test.js
@@ -5,16 +5,79 @@ var baseUrl = "http://34.209.33.250:4200";
 
 var util = require('util');
 
-describe('return user', function () {
-    it('return user', function (done) {
-        request.post({
-            url: baseUrl + '/login',
-            form: {
-                email: "linxiaoran2018@gmail.com",
-                password: "Lx860210!"
-            }}, function (err, res, body) {
-            expect(res.statusCode).to.equal(200);
-            done();
+describe('mocha test for API', function () {
+    describe('return user info from /login', function() {
+        it('return user', function (done) {
+            request.post({
+                url: baseUrl + '/login',
+                form: {
+                    email: "linxiaoran2018@gmail.com",
+                    password: "Lx860210!"
+                }
+            }, function (err, res, body) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
+        })
+    })
+
+    describe('return profile info from /profile', function() {
+        it('return profile', function (done) {
+            request.post({
+                url: baseUrl + '/login',
+                form: {
+                    email: "linxiaoran2018@gmail.com",
+                    password: "Lx860210!"
+                }
+            }, function (err, res, body) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
+        })
+    })
+
+    describe('return profile info from /postproject', function() {
+        it('return project', function (done) {
+            request.post({
+                url: baseUrl + '/login',
+                form: {
+                    email: "linxiaoran2018@gmail.com",
+                    password: "Lx860210!"
+                }
+            }, function (err, res, body) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
+        })
+    })
+
+    describe('return profile info from /getbidlist', function() {
+        it('return bidlist', function (done) {
+            request.post({
+                url: baseUrl + '/login',
+                form: {
+                    email: "linxiaoran2018@gmail.com",
+                    password: "Lx860210!"
+                }
+            }, function (err, res, body) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
+        })
+    })
+
+    describe('return profile info from /allprojects', function() {
+        it('return all projects', function (done) {
+            request.post({
+                url: baseUrl + '/login',
+                form: {
+                    email: "linxiaoran2018@gmail.com",
+                    password: "Lx860210!"
+                }
+            }, function (err, res, body) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
         })
     })
 })
